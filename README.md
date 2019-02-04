e28baa59c04e25215bf820d25c623ee0d494a336
Automedon <42299107+Automedon@users.noreply.github.com>
2019-02-04 19:44:42 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mego(no)todolist
---
+++ precache-manifest.45ad45ed9218498b3430409b8b90bc3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6be4ad2525f0086cd8ef",
+    "url": "/./static/css/main.894a7f1d.chunk.css"
+  },
+  {
+    "revision": "6be4ad2525f0086cd8ef",
+    "url": "/./static/js/main.6be4ad25.chunk.js"
+  },
+  {
+    "revision": "4fa78c522b0478ba1ce6",
+    "url": "/./static/css/1.db088c64.chunk.css"
+  },
+  {
+    "revision": "4fa78c522b0478ba1ce6",
+    "url": "/./static/js/1.4fa78c52.chunk.js"
+  },
+  {
+    "revision": "8c91a21b86f41b9c9369",
+    "url": "/./static/js/runtime~main.8c91a21b.js"
+  },
+  {
+    "revision": "7c64b028bb7345208422488fbfdc48e1",
+    "url": "/./index.html"
+  }
+];
